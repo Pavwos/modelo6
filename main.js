684e712a3a7bb1d0a340e1bfce0982ca921fd1de
Pavwos <rspavanelli@gmail.com>
2024-04-03 00:00:14 -0300

    inicio

---
+++ main.js
@@ -0,0 +1,14 @@
+const display = document.getElementById('display')
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = ''
+
+}
+
+function calculate(){
+    display.value = eval(display.value)
+}
